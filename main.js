e2c5734a1af08efcdce04770e232e28fe9ea5bd1
Lomme Tack <lomme.tack@student.howest.be>
2025-01-21 23:54:52 +0100

    css mobile

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import "/css/style.css";
+
+const menuToggle = document.querySelector('.header__menu-toggle');
+const nav = document.querySelector('.header__nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+});
