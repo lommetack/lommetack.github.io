eb06720bda0c093b6691cdd3b41434baa9837140
Lomme Tack <lomme.tack@student.howest.be>
2025-01-25 02:13:43 +0100

    commit 25/01

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ const nav = document.querySelector('.header__nav');
 menuToggle.addEventListener('click', () => {
     nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
 });
+
+
+// const intro = document.querySelector('.intro');
+
+// intro.addEventListener('mousemove', (event) => {
+//   const rect = intro.getBoundingClientRect();
+//   const x = ((event.clientX - rect.left) / rect.width) * 100;
+//   const y = ((event.clientY - rect.top) / rect.height) * 100;
+
+//   intro.style.backgroundPosition = `${x}% ${y}%`;
+// });
+
